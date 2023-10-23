24fad9863c6bb39e546d19817721c5f8e99530ff
SNQ-2001 <info@taishin-miyamoto.com>
2023-10-23 19:46:47 +0900

    feat: プロジェクトを作成

---
+++ AutoVersionIncrementWithGitHubAction/AutoVersionIncrementWithGitHubActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoVersionIncrementWithGitHubActionApp.swift
+//  AutoVersionIncrementWithGitHubAction
+//
+//  Created by 宮本大新 on 2023/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AutoVersionIncrementWithGitHubActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
